cf7ee361dcca4779f23aeb11a66141280d5f0b21
Khuc Nam Hai <khnambk@gmail.com>
2017-09-07 14:05:26 +0700

    Create valid_parentheses_clever_1.js

---
+++ valid_parentheses_clever_1.js
@@ -0,0 +1,9 @@
+function validParentheses(parens){
+  var indent = 0;
+  
+  for (var i = 0 ; i < parens.length && indent >= 0; i++) {
+    indent += (parens[i] == '(') ? 1 : -1;    
+  }
+  
+  return (indent == 0);
+}
